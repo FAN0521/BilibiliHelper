f44a85c263f1f90eea424c234fc1e30a570c11d7
fan <shfanwen@cn.ibm.com>
2019-06-07 01:59:36 +0800

    注释掉领瓜子、扭蛋和舰长亲密度

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ const app = async () => {
     await module_auth()
     await module_tasks()
     await module_heart()
-    await module_silver()
+    //await module_silver()
     await module_group()
-    await module_capsule()
+    //await module_capsule()
     await module_giftsend()
     await module_dailybag()
     await module_silver2coin()
-    await module_guard()
+    //await module_guard()
     await sleep(1000)
   }
 }
